5b849267e218427e0c41382a39c861c4f40a6a68
Antonio Rosales <antoniorosalesmartinez90@gmail.com>
2024-02-21 11:52:37 +0100

    feat js

---
+++ JAVASCRIPT/1.conceptos.js
@@ -0,0 +1,29 @@
+// VARIABLES
+// Esto es un comentario de linea
+/*
+Esto
+es un 
+comentario
+de 
+bloque
+*/
+
+//Declarar una variable
+var ciudad;
+// Asignar un valor a una variable
+ciudad = "Madrid";
+//Declarar y asignar
+var pais = "Japón";
+//LET Y CONST
+
+let x = 10;
+x = 11;
+x = "Patata";
+
+const y = 5;
+console.log(y);
+
+//Maneras de escribir
+const nombre = "Antonio";
+// camelCase o barras bajas es la manera de escribir el nombre de nuestras variables
+const primerApellido = "Rosales";
---
+++ JAVASCRIPT/2.tiposdedatos.js
@@ -0,0 +1,36 @@
+// String
+const frase = "Hola, estamos aprendiendo a programar";
+const citaFamosa = 'Shakespeare dijo: "Ser o no ser"';
+
+//Number
+const edad = 33;
+const decimal = 50.22;
+
+//Boolean
+const aprobado = true;
+const asistente = false;
+
+//Array
+const alimentos = [
+  "Papas",
+  "Vinagre de arroz",
+  "Lejia",
+  "Sopa de letras",
+  "Algas",
+];
+const numeros = [1, 2, 3, 4, 5, 100];
+
+//Objetos
+const televisor = {
+  marca: "Synetech",
+  precio: 5.5,
+  pulgadas: 80,
+  color: "gris",
+  pagada: true,
+  comentarios: ["Muy bueno", "Calidad precio guay", "muy bonito"],
+};
+
+//Null
+const x = null
+//Undefined
+const y = undefined
