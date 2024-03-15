aac8faebffe4e75fb827a85739b4d6c718b400d7
Antonio Rosales <desarrollowebfp@thepower.education>
2024-03-15 10:34:38 +0100

    feat demo

---
+++ VITE/02.demo/data/materiales.js
@@ -0,0 +1,58 @@
+const materiales = [
+  {
+    nombre: "Cama para perro",
+    marca: "Arf Pets",
+    precio: 29.99,
+    imagen: "https://m.media-amazon.com/images/I/51G70pS27IL.jpg",
+  },
+  {
+    nombre: "Comedero y bebedero elevado",
+    marca: "HoneyGuaridan",
+    precio: 17.99,
+    imagen:
+      "https://media.adeo.com/marketplace/MKP/86722570/3e73b1f6a8244a49815c7d2f3f46c1ec.jpeg?width=650&height=650&format=jpg&quality=80&fit=bounds",
+  },
+  {
+    nombre: "Collar con correa",
+    marca: "Frienda",
+    precio: 14.99,
+    imagen:
+      "https://www.luscoandfusco.es/wp-content/uploads/2019/05/combo-1400x1050.jpg",
+  },
+  {
+    nombre: "Arnés para perros",
+    marca: "Julius-K9",
+    precio: 49.99,
+    imagen:
+      "https://m.media-amazon.com/images/I/61gEfldpRKL._AC_UF894,1000_QL80_.jpg",
+  },
+  {
+    nombre: "Juguetes para perros",
+    marca: "Kong",
+    precio: 12.99,
+    imagen:
+      "https://storage.googleapis.com/catalog-pictures-carrefour-es/catalog/pictures/hd_510x_/8719138813957_1.jpg",
+  },
+  {
+    nombre: "Champú para perros",
+    marca: "Paws & Pals",
+    precio: 9.99,
+    imagen:
+      "https://www.petsworldmarket.com/2247-large_default/trixie-champu-perro-aceite-natural-250-ml.jpg",
+  },
+  {
+    nombre: "Cepillo para perros",
+    marca: "FURminator",
+    precio: 24.99,
+    imagen:
+      "https://m.media-amazon.com/images/I/7176y++DW-L._AC_UF894,1000_QL80_.jpg",
+  },
+  {
+    nombre: "Toallitas húmedas para perros",
+    marca: "PetSafe",
+    precio: 7.99,
+    imagen: "https://m.media-amazon.com/images/I/81FG+F0mf5L.jpg",
+  },
+];
+
+export default materiales;
--- VITE/02.demo/main.js
+++ VITE/02.demo/main.js
@@ -1,1 +1,47 @@
+//Importamos los estilos
 import "./style.css";
+//Importamos los arrays de perros y materiales
+import perros from "./data/perros.js";
+import materiales from "./data/materiales.js";
+
+//Vamos a implementar el cambiador de temas
+//Recuperamos el botón y lo almacenamos en una constante
+const temaBtn = document.querySelector("#tema");
+//Le vamos a añadir un evento click que le añada y le quite una clase al body llamda dark
+temaBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+});
+
+//Recuperamos la seccion de perros y materiales que hemos creado previamente en el HTML
+const perrosSection = document.querySelector("#perrosSection");
+const materialesSection = document.querySelector("#materialesSection");
+
+//Por cada una de las secciones vamos a recorrer con un bucle su respectivo array e inyectar dinamicamente cada uno de los objetos dentro de las secciones. Para ello podemos utilizar una etiqueta article con las semanticas que elijamos dentro de ellas.
+
+//Perros
+for (const perro of perros) {
+  //Creamos un article vacio en JS
+  const article = document.createElement("article");
+  //Le rellenamos el HTML al article con innerHTML
+  article.innerHTML = `
+  <h3>${perro.raza}</h3>
+  <h4>${perro.tamaño} - ${perro.peso_medio}kg</h4>
+  <img src=${perro.imagen} alt=${perro.raza}/>
+  `;
+  //Inyectamos todos estos articles dentro de la seccion perros
+  perrosSection.appendChild(article);
+}
+
+//Metariales
+for (const material of materiales) {
+  //Creamos un article vacio en JS
+  const article = document.createElement("article");
+  //Le rellenamos el HTML al article con innerHTML
+  article.innerHTML = `
+  <h3>${material.nombre}</h3>
+  <h4>${material.marca} - ${material.precio}€</h4>
+  <img src=${material.imagen} alt=${material.nombre}/>
+  `;
+  //Inyectamos todos estos articles dentro de la seccion materiales
+  materialesSection.appendChild(article);
+}
